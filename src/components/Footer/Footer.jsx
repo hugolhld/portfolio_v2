e585c1d546cb746c67fbc0834e28610cd8691936
hugolhld <hugo.lhernould@gmail.com>
2023-11-29 23:20:37 +0100

    update for other computer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-black'>Footer</div>
+    <div className='bg-black py-4'>Footer</div>
   )
 }
 
--- src/screens/Skills.jsx
+++ src/screens/Skills.jsx
@@ -1,18 +1,80 @@
 import React from 'react'
 
 const Skills = () => {
+
+    const wHeight = window.screenX * 0.25
+
+    console.log(wHeight)
+
   return (
     <section className='w-full h-screen'>
-        <div>
-            <div className=" w-1/5 h-1/3 top border-r-2 border-b-2">
+        <div className='w-full h-full flex flex-wrap mx-auto'>
+            <div className={`w-1/4 h-[${wHeight}px] border`}>
+                
+            </div>
+            <div className={`w-1/4 h-[${wHeight}px] border`}>
+
+            </div>
+            <div className={`w-1/4 h-[${wHeight}px] border`}>
+
+            </div>
+            <div className={`w-1/4 h-[${wHeight}px] border`}>
+
+            </div>
+            <div className={`w-1/4 h-[${wHeight}px] border`}>
+                
+            </div>
+            <div className={`w-1/4 h-[${wHeight}px] border`}>
+
+            </div>
+            <div className={`w-1/4 h-[${wHeight}px] border`}>
+
+            </div>
+            <div className={`w-1/4 h-[${wHeight}px] border`}>
+
+            </div>
+            
+          
+           
+            {/* <div className=" w-1/5 h-1/3 top border-t-2 border-r-2 border-b-2">
 
             </div>
             <div className=" w-1/5 h-1/3 middle border-r-2">
 
             </div>
-            <div className=" w-1/5 h-1/3 bottom border-t-2 border-r-0">
+            <div className=" w-1/5 h-1/3 bottom border-b-2 border-t-2 border-r-2">
 
             </div>
+            
+            <div className=" w-1/5 h-1/3 top border-t-2 border-r-2 border-b-2">
+
+            </div>
+            <div className=" w-1/5 h-1/3 middle border-r-2">
+
+            </div>
+            <div className=" w-1/5 h-1/3 bottom border-b-2 border-t-2 border-r-2">
+
+            </div>
+            
+            <div className=" w-1/5 h-1/3 top border-t-2 border-r-2 border-b-2">
+
+            </div>
+            <div className=" w-1/5 h-1/3 middle border-r-2">
+
+            </div>
+            <div className=" w-1/5 h-1/3 bottom border-b-2 border-t-2 border-r-2">
+
+            </div>
+            
+            <div className=" w-1/5 h-1/3 top border-t-2 border-r-2 border-b-2">
+
+            </div>
+            <div className=" w-1/5 h-1/3 middle border-r-2">
+
+            </div>
+            <div className=" w-1/5 h-1/3 bottom border-b-2 border-t-2 border-r-2">
+
+            </div> */}
         </div>
     </section>
   )
