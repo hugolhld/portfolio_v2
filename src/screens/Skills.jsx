82cba88c5c5cc4bf0c84b7b854fde8972d9ccee9
hugolhld <hugo.lhernould@gmail.com>
2023-11-30 18:42:24 +0100

    for other computer

--- src/screens/Skills.jsx
+++ src/screens/Skills.jsx
@@ -8,8 +8,35 @@ const Skills = () => {
 
   return (
     <section className='w-full h-screen'>
-        <div className='w-full h-full flex flex-wrap mx-auto'>
-            <div className={`w-1/4 h-[${wHeight}px] border`}>
+        <div className='w-full h-full flex flex-wrap mx-auto bg-white text-white odd:bg-red-400'>
+            <div className="w-1/4 border border-black flex justify-center items-center">
+              1
+           </div>
+            <div className="w-1/4 border border-black flex justify-center items-center">
+              2
+            </div>
+            <div className="w-1/4 border border-black flex justify-center items-center">
+              3
+            </div>
+            <div className="w-1/4 border border-black flex justify-center items-center">
+              4
+            </div>
+            <div className="w-1/4 border border-black flex justify-center items-center">
+              5
+            </div>
+            <div className="w-1/4 border border-black flex justify-center items-center">
+              6
+            </div>
+            <div className="w-1/4 border border-black flex justify-center items-center">
+              7
+            </div>
+            <div className="w-1/4 border border-black flex justify-center items-center">
+              8
+            </div>
+            <div className="w-1/4 border border-black flex justify-center items-center">
+              9
+            </div>
+            {/* <div className={`w-1/4 h-[${wHeight}px] border`}>
                 
             </div>
             <div className={`w-1/4 h-[${wHeight}px] border`}>
@@ -32,7 +59,7 @@ const Skills = () => {
             </div>
             <div className={`w-1/4 h-[${wHeight}px] border`}>
 
-            </div>
+            </div> */}
             
           
            
